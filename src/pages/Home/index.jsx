0b9a26f5a1d20f3001ad15b8aac585c06cfb4189
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-11-21 10:54:25 +0400

    servicecard section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,23 @@
+import { Container, Grid } from "@mui/material"
+import HomeServiceCard from "../../components/HomeServiceCard"
+
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Container>
+                <Grid container spacing={3}>
+                    {
+                        ['', '', '', '', '', ''].map(() => {
+                            return (
+                                <Grid item xs={12} sm={6} md={4}>
+                                    <HomeServiceCard />
+                                </Grid>
+                            )
+                        })
+                    }
+                </Grid>
+            </Container>
+        </div>
     )
 }
 export default Home
