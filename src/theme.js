5c580d0e627c9365cace0601e60be306a135499c
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-11-21 10:54:04 +0400

    add secondary light

--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,11 @@ import { createTheme } from "@mui/material";
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#435072'
+            main: '#435072',
         },
         secondary: {
-            main: '#606F84'
+            main: '#606F84',
+            light: 'rgba(67, 80, 114, 0.1)'
         }
     }
 })
