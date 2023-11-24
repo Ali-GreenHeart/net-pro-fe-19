9946a32b9f8b9f5b2e6d30a6a93d2383b210e79c
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-11-24 14:14:40 +0300

    Props logic is added to NetProIcon

--- src/components/netProIcon/index.jsx
+++ src/components/netProIcon/index.jsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
 
-const NetProIcon = () => {
+const NetProIcon = ({bgColor, afterBgColor, top, word1Color, word2Color}) => {
     return (
         <Box
             sx={{
@@ -9,10 +9,10 @@ const NetProIcon = () => {
                 display: 'inline-block',
                 width: 85,
                 height: 65,
-                bgcolor: 'secondary.main',
+                bgcolor: bgColor,
                 position: 'relative',
                 transform: 'scale(1.2)',
-                top: 10,
+                top: top,
                 zIndex: 1,
                 "&::after": {
                     content: '""',
@@ -20,7 +20,7 @@ const NetProIcon = () => {
                     height: 75,
                     left: 18,
                     top: -13,
-                    backgroundColor: '#F4F4F4',
+                    backgroundColor: afterBgColor,
                     position: 'absolute',
                     transform: 'rotate(45deg)',
                     zIndex: 0,
@@ -29,8 +29,8 @@ const NetProIcon = () => {
             }}
         >
             <Box sx={{ position: 'absolute', left: 25, top:-7 }}>
-                <Typography color="initial" sx={{ position: 'relative', zIndex: 5, fontSize: 32, fontWeight: 600, color: 'primary.main' }}>NETPRO</Typography>
-            <Typography variant="body1" color="initial" sx={{ position: 'relative', zIndex: 5,bottom:10, fontWeight: 600, color: 'primary.light' }}>GROUP</Typography>
+                <Typography color="initial" sx={{ position: 'relative', zIndex: 5, fontSize: 32, fontWeight: 600, color: word1Color }}>NETPRO</Typography>
+            <Typography variant="body1" color="initial" sx={{ position: 'relative', zIndex: 5,bottom:10, fontWeight: 600, color: word2Color }}>GROUP</Typography>
         </Box>
         </Box >
     )
