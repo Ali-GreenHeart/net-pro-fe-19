88e56f7763e7373c293b21b6f916f49ae4eaf701
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-11-25 20:52:32 +0300

    Import to SwiperComponent

--- src/components/homephoto/index.jsx
+++ src/components/homephoto/index.jsx
@@ -6,26 +6,11 @@ import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import { Margin } from '@mui/icons-material';
+import SwiperComponent from '../Swiper'
 
 const HomePhoto = () => {
     return (
-        <Box sx={{ height: '80vh', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: "center", alignItems: 'flex-start', gap: 3 }}>
-            <Typography variant="h3" color="primary.main" component={'h1'} width={'60%'} fontWeight={'600'}>
-                Avadanlıqların Quraşdırılması
-                və Konfiqurasiyası
-            </Typography>
-            <Box sx={{ bgcolor: 'white', borderRadius: '12px', padding: '2px' }} >
-                <Button variant={'contained'} sx={{ bgcolor: '#FFFFFF', color: 'primary.main', borderRadius: '10px', fontWeight: '600', border: '1px solid #435072' }}>
-                    Daha Ətraflı
-                    <KeyboardDoubleArrowRightIcon />
-                </Button>
-            </Box>
-            <Box style={{marginTop:'2rem'}}>
-                <Stack spacing={2}>
-                    <Pagination count={3} color="primary" />
-                </Stack>
-            </Box>
-        </Box >
+        <SwiperComponent/>
     )
 }
 
