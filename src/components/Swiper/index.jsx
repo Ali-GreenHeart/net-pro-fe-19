9443211bfb20455b582dfce7cd20c8e641904df3
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-05 10:11:10 +0300

    some import corrections in Swiper

--- src/components/Swiper/index.jsx
+++ src/components/Swiper/index.jsx
@@ -10,7 +10,7 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { ArrowLeft, ArrowRight } from '@mui/icons-material';
-import { Box, Button, Stack } from '@mui/material';
+import { Box, Button, Stack, Container } from '@mui/material';
 import { Navigation, Pagination } from 'swiper/modules';
 import { HomeSlides } from './HomeSlides.jsx'
 
@@ -24,34 +24,34 @@ export default function SwiperComponent() {
         <Box sx={{
             position: 'relative',
         }}>
-            <Stack
-                gap={2}
-                flexDirection="row"
-                sx={{
-                    position: 'absolute',
-                    zIndex: 100,
-                    bottom: 60,
-                    left: 40,
-                    color: 'primary.main',
-                }}>
-                <h1> {realIndex} /  5 </h1>
-            </Stack>
-            <Stack
-                gap={2}
-                flexDirection="row"
-                sx={{
-                    position: 'absolute',
-                    zIndex: 100,
-                    bottom: 35,
-                    right: 40
-                }}>
-                <Button variant="contained" ref={prevRef}>
-                    <ArrowLeft />
-                </Button>
-                <Button variant="contained" ref={nextRef}>
-                    <ArrowRight />
-                </Button>
-            </Stack>
+                <Stack
+                    gap={2}
+                    flexDirection="row"
+                    sx={{
+                        position: 'absolute',
+                        zIndex: 100,
+                        bottom: 60,
+                        left: 40,
+                        color: 'primary.main',
+                    }}>
+                    <h1> {realIndex} /  5 </h1>
+                </Stack>
+                <Stack
+                    gap={2}
+                    flexDirection="row"
+                    sx={{
+                        position: 'absolute',
+                        zIndex: 100,
+                        bottom: 35,
+                        right: 40
+                    }}>
+                    <Button variant="contained" ref={prevRef}>
+                        <ArrowLeft />
+                    </Button>
+                    <Button variant="contained" ref={nextRef}>
+                        <ArrowRight />
+                    </Button>
+                </Stack>
             <Swiper
                 onRealIndexChange={({ realIndex }) => {
                     setRealIndex(realIndex + 1)
