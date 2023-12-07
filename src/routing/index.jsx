0781a477ac63351e9a0724c90d628897076ae98a
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-07 09:39:07 +0300

    Services Page's routine

--- src/routing/index.jsx
+++ src/routing/index.jsx
@@ -7,20 +7,19 @@ import HomeCustomers from "../components/HomeCustomers"
 import React from 'react'
 import ProductSingle from "../pages/ProductSingle"
 import Products from "../pages/Products"
+import ServicesP from "../pages/Services"
 
 const WebRouting = () => {
     return (
-        <>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/products" element={<Products/>}/>
-                <Route path="/icon" element={<NetProIcon />} />
                 <Route path="/homecustomers" element={<HomeCustomers />} />
                 <Route path="/products/:slug" element={<ProductSingle />} />
+                <Route path="/services" element={<ServicesP />} />
             </Routes>
-        </>
     )
 }
 export default WebRouting
