0d63258569fc294e3fe6a5e81ab91e5fbd381698
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-07 09:38:10 +0300

    add images info of services

--- src/mockData/services.js
+++ src/mockData/services.js
@@ -2,32 +2,38 @@ const services = [
     {
         id: 0,
         title: 'Avadanlıqların quraşdırılması və konfiqurasiyası',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.'
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.',
+        image:'/ServicesSection/image1.png'
     },
     {
         id: 1,
         title: 'Daimi nəzarət',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.'
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.',
+        image:'/ServicesSection/image2.png'
     },
     {
         id: 2,
         title: 'Avadanlıqların vaxtında dəyişdirilməsi',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.'
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.',
+        image:'/ServicesSection/image3.png'
     },
     {
         id: 4,
         title: 'İT xidmətlər',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.'
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.',
+        image:'/ServicesSection/image4.png'
     },
     {
         id: 5,
         title: 'Helpdesk xidmətlər',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.'
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.',
+        image:'/ServicesSection/image5.png'
     },
     {
         id: 6,
         title: 'Təhlükəsizlik xidmətləri',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.'
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Urna diam ullamcorper aliquet non, ut id sit eros integer.',
+        image:'/ServicesSection/image6.png'
     }
 ]
 export default services;
