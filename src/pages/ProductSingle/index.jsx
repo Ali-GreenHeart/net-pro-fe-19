679d6b5065569678a3f8324c669cedd07424f5e4
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-10 18:35:14 +0300

    delete unused things and add more designs and functions

--- src/pages/ProductSingle/index.jsx
+++ src/pages/ProductSingle/index.jsx
@@ -3,123 +3,12 @@ import PageContainer from "../../components/PageContainer"
 import { Box, Container, Grid, Pagination, Paper, Stack, Typography } from "@mui/material"
 import { Link } from "react-router-dom"
 import { useEffect, useState } from "react"
-
-const data = [
-    {
-        id: 0,
-        slug: 'uninterruptible-power-supplies',
-        title: 'Uninterruptible Power Supplies',
-        devices: [
-            { name: 'name1' },
-            { name: 'nUninterruptibleame2' },
-            { name: 'Uninterruptiblename3' },
-            { name: 'nUninterruptibleame4' },
-            { name: 'nUninterruptibleame5' },
-            { name: 'nUninterruptibleame6' },
-            { name: 'name7Uninterruptible' },
-            { name: 'nUninterruptibleame8' },
-            { name: 'Uninterruptiblename9' },
-            { name: 'name10Uninterruptible' },
-            { name: 'name11Uninterruptible' },
-            { name: 'name12Uninterruptible' }
-        ]
-    },
-    {
-        id: 1,
-        slug: 'dc-power-systems',
-        title: 'DC Power Systems',
-        devices: [
-            { name: 'DCname1' },
-            { name: 'DCname2' },
-            { name: 'DCname3' },
-            { name: 'DCname4' },
-            { name: 'naDCme5' },
-            { name: 'name6DC' },
-            { name: 'DCname7' },
-            { name: 'DCname8' },
-            { name: 'name9DC' },
-            { name: 'name10DC' },
-            { name: 'name11DC' },
-            { name: 'name12DC' }
-        ]
-    },
-    {
-        id: 2,
-        slug: 'power-distribution',
-        title: 'Power Distribution',
-        devices: [
-            { name: 'name1Power' },
-            { name: 'Powername2' },
-            { name: 'Powername3' },
-            { name: 'Powername4' },
-            { name: 'name5Power' },
-            { name: 'name6Power' },
-            { name: 'name7Power' },
-            { name: 'name8Power' },
-            { name: 'name9Power' },
-            { name: 'name10Power' },
-            { name: 'name11Power' },
-            { name: 'name12Power' }
-        ]
-    },
-    {
-        id: 3,
-        slug: 'industrial-ac-and-dc-systems',
-        title: 'Industrial AC and DC Systems',
-        devices: [
-            { name: 'Industrialname1' },
-            { name: 'Industrialname2' },
-            { name: 'Industrialname3' },
-            { name: 'Industrialname4' },
-            { name: 'Industrialname5' },
-            { name: 'name6Industrial' },
-            { name: 'name7Industrial' },
-            { name: 'Industrialname8' },
-            { name: 'name9Industrial' },
-            { name: 'name10Industrial' },
-            { name: 'name11Industrial' },
-            { name: 'name12Industrial' }
-        ]
-    },
-    {
-        id: 4,
-        slug: 'static-transfer-switches',
-        title: 'Static Transfer Switches',
-        devices: [
-            { name: 'name1Static' },
-            { name: 'Staticname2' },
-            { name: 'Staticname3' },
-            { name: 'Staticname4' },
-            { name: 'name5Static' },
-            { name: 'name6Static' },
-            { name: 'name7Static' },
-            { name: 'name8Static' },
-            { name: 'name9Static' },
-            { name: 'name10Static' },
-            { name: 'name11Static' },
-            { name: 'name12Static' }
-        ]
-    },
-    {
-        id: 5,
-        slug: 'power-control-and-monitoring',
-        title: 'Power Control and Monitoring',
-        devices: [
-            { name: 'name1Power' },
-            { name: 'Powername2' },
-            { name: 'Powername3' },
-            { name: 'Powername4' },
-            { name: 'name5Power' },
-            { name: 'name6Power' },
-            { name: 'name7Power' },
-            { name: 'name8Power' },
-            { name: 'name9Power' },
-            { name: 'name10Power' },
-            { name: 'name11Power' },
-            { name: 'name12Power' }
-        ]
-    },
-]
+import { productsS } from "../../mockData/products"
+import * as React from 'react';
+import Divider from '@mui/material/Divider';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import DevicesCards from "./DevicesCards"
 
 const ProductSingle = () => {
     const [id, setId] = useState(0)
@@ -127,64 +16,68 @@ const ProductSingle = () => {
     const { slug } = useParams()
 
     useEffect(() => {
-        const slugItemId = data.find((it) => it.slug === slug).id
+        const slugItemId = productsS.find((it) => it.slug === slug).id
         setId(slugItemId)
         setPage(1)
     }, [slug])
 
 
-    const item = data.find((_item) => _item.id === id)
+    const item = productsS.find((_item) => _item.id === id)
+    console.log(item);
     return (
         <PageContainer bannerTitle="VERTIV">
-            <Container>
-                <Grid container my={3}>
-                    <Grid xs={12} sm={6} md={4} item>
-                        <Paper elevation={0}
-                            sx={(theme) => ({
-                                borderRadius: 2,
-                                border: `1px solid ${theme.palette.primary.light}`,
-                            })}
-                        >
-                            <Typography
-                                variant="h5"
-                                color="white"
-                                bgcolor="primary.main"
-                            // sx={{
-                            //     borderTopLeftRadius: 'inherit',
-                            //     borderTopRightRadius: 'inherit',
-                            // }}
-                            >Category</Typography>
-                            <Stack>
-                                {
-                                    data.map(({ id, slug, title }) => {
-                                        return (
-                                            <Link key={id} to={`/products/${slug}`}>{title}</Link>
-                                        )
-                                    })
-                                }
-                            </Stack>
+            <Container sx={{py:5}}>
+                <Grid container my={3} spacing={{sm:2,md:1, xl:5}}>
+                    <Grid xs={12}md={4} item>
+                        <Paper sx={{ width: 400, maxWidth: '100%' }}>
+                            <MenuList subheader={
+                                <Paper sx={{ bgcolor: "primary.main", color: 'info.main', p: 3, fontWeight: 600, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, letterSpacing: 1, mb: 2 }}>Caterogy</Paper>
+                            }>
+                                <Stack justifyContent={'space-evenly'} flexWrap={'wrap'}>
+                                    {
+                                        productsS.map(({ id, slug, title }) => {
+                                            return (
+                                                <>
+                                                    <MenuItem disableRipple disableTouchRipple>
+                                                        <Link key={id} to={`/products/${slug}`} style={{
+                                                            textDecoration: 'none',
+                                                            color: item.id === id ? '#435072' : 'rgba(86, 96, 122, 0.5)', fontWeight: 'bold',
+                                                            padding: 5,
+                                                            width: '100%',
+                                                            textWrap: 'wrap'
+                                                        }}>
+                                                            {title}
+                                                        </Link>
+                                                    </MenuItem>
+                                                    {id !== 5 ?
+                                                        <Divider variant="middle" /> : null}
+                                                </>
+                                            )
+                                        })
+                                    }
+                                </Stack>
+                            </MenuList>
                         </Paper>
                     </Grid>
-                    <Grid xs={12} sm={6} md={8} item>
-                        {
-                            <Typography>{item.title}</Typography>
-                        }
-                        {
-                            item.devices.slice((page - 1) * 3, page * 3).map(({ name }) => {
-                                return (
-                                    <Box py={4} key={name}>
-                                        <Typography>{name}</Typography>
-                                    </Box>
-                                )
-                            })
-                        }
-                        <Pagination
-                            count={item.devices.length / 3}
-                            page={page}
-                            onChange={(_, value) => {
-                                setPage(value)
-                            }}
-                        />
+                    <Grid sm={12} md={8} item >
+                        <Stack justifyContent={'space-between'} flexWrap={'wrap'} spacing={4}>
+                            <Typography variant="h5" color="primary">{item.title}</Typography>
+                            <Typography variant="body1" color="primary">{item.body}</Typography>
+                            {
+                                item.devices.slice((page - 1) * 2, page * 2).map(({ id, name, body, image }) => {
+                                    return (
+                                        <DevicesCards name={name} id={id} body={body} image={image}/>
+                                    )
+                                })
+                            }
+                            <Pagination
+                                count={item.devices.length / 2}
+                                page={page}
+                                onChange={(_, value) => {
+                                    setPage(value)
+                                }}
+                            />
+                        </Stack>
                     </Grid>
                 </Grid>
             </Container>
