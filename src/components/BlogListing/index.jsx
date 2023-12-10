f1b33b7cf52dcc7d4257fc084a1fce01cb9821a5
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-12-10 23:20:38 +0300

    formatting

--- src/components/BlogListing/index.jsx
+++ src/components/BlogListing/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {Typography, Grid} from '@mui/material'
+import { Typography, Grid } from '@mui/material'
 import BlogCards from './BlogCards'
 
 const BlogListing = () => {
     return (
         <>
-            <Typography variant="h4" color="primary" sx={{m:2}}>Bloq</Typography>
+            <Typography variant="h4" color="primary" sx={{ m: 2 }}>Bloq</Typography>
             <Grid container>
                 <BlogCards />
             </Grid>
